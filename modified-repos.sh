922bd54f256f0f6dca4fd1668362cb35d404197d
Per Henden <per.christian.henden@nav.no>
2025-11-21 09:09:41 +0100

    fjern logging

--- modified-repos.sh
+++ modified-repos.sh
@@ -2,8 +2,7 @@
 find . -type d -name ".git" -mindepth 2 -maxdepth 2| while read repo; do
     pushd "$(dirname "$repo")" > /dev/null 2>&1
     if [ -n "$(git status --porcelain | grep -v "^??")" ]; then
-        echo $PWD
-        echo $(git status --porcelain | grep -v "^??") fi
+        echo $(basename "$PWD")
     fi
     popd > /dev/null 2>&1
 done
